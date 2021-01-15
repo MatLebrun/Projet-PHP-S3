59019f2b4ad49d34af923dec704cb1b774b077ff
Hugo <hugo.clergue@etu.univ-amu.fr>
2021-01-15 17:15:11 +0100

    Create index.php

---
+++ Code/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Bienvenue sur le réseau social de Vanestar" />
+
+    <link href="styles/common.css" rel="stylesheet">
+    <title>Accueil : Vanestarre</title>
+    <link rel="icon" href="">   
+</head>
+
+<body>
+
+<div class="topnav">
+  <a href="#home">Vanestarre</a>
+  <a><form>
+  <input class="tagsearch" type="text" placeholder="Recherche">
+  </form></a>
+  <a href="#login">Se connecter</a>
+</div>
+<div class="content">
+
+</div>
+
+
+</body>
+</html>
