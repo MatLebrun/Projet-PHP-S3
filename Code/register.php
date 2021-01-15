00b5e80ec762a3d18b8f906f0b9f1a3fae7a0157
MatLebrun <mathieu.lebrun@etu.univ-amu.fr>
2021-01-15 14:57:32 +0100

    register

---
+++ Code/register.php
@@ -0,0 +1,61 @@
+
+<!-- <?php
+
+require __DIR__.'/utils.inc.php';
+
+define('PAGE_TITLE', 'Accueil');
+
+start_page(PAGE_TITLE);
+
+?> -->
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>S'inscrire</title>
+
+
+    <link href="/styles/register.css" rel="stylesheet">
+    <script type="text/javascript" src="monscript.js"></script>
+
+</head>
+    <body>
+
+    <h1>fzf</h1>
+
+
+        <form action="data-processing.php" method="post">
+            <input type="text" name="identifiant" placeholder="Identifiant"> <br>
+            
+            <input type="radio" name="sexe" value="F" id="Femme">femme
+            <input type="radio" name="sexe" id="H" value="Homme">homme <br>
+
+            <input type="email" name="email" placeholder="Email"> <br>
+
+            <input type="password" name="password" placeholder="Mot de passe">
+            <input type="password" name="password2" placeholder="Répetez votre mot de passe">
+
+            <input type="text" name="telephone" placeholder="Téléphone"> <br>
+
+            <select name="pays">
+                <option value="France">France</option>
+                <option value="France">Russie</option>
+                <option value="France">Espagne</option>
+            </select> <br>
+
+            <input type="checkbox" name="cgu">Conditions générales d'utilisation <br>
+
+            <button type="submit" name="submit">S'inscrire</button>
+
+        </form>
+
+    
+</body>
+</html>
+
+<!-- <?php
+    end_page();
+?> -->
