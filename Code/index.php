5638ae911d140349f9bd9c154e10c1ca5fdbccd9
MatLebrun <mathieu.lebrun@etu.univ-amu.fr>
2021-01-15 14:17:12 +0100

    Update index.php

--- Code/index.php
+++ Code/index.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Bienvenue sur le réseau social de Vanestar" />
 
-    <link href="" rel="stylesheet">
+    <link href="../styles/common.css" rel="stylesheet">
+    <link href="../styles/index.css" rel="stylesheet">
+
     <title>Accueil : Vanestar</title>
     <link rel="icon" href="">
 
