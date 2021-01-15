7e47c5faa0da11b86588c7a73d51bc4b0ac559d5
MatLebrun <mathieu.lebrun@etu.univ-amu.fr>
2021-01-15 16:33:20 +0100

    sinscrire

--- Code/register.php
+++ Code/register.php
@@ -1,3 +1,7 @@
+<?php
+        session_start();
+    ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,40 +10,74 @@
     <title>S'inscrire</title>
 
 
-    <link href="/styles/register.css" rel="stylesheet">
+    <link href="./styles/register.css" rel="stylesheet">
     <script type="text/javascript" src="monscript.js"></script>
 
 </head>
     <body>
 
+
+
+
+
+    <!--    Menu Principal du site  -->
+    <nav id="menu_deroulant"> 
+      <ul id="deroulant">
+        <li>
+          <a href="">accueil</a>
+        <li>
+          <a href="">login</a>
+        <li>
+            <a href="">sinscrire</a>
+        <li>
+            <a href="">a propos</a>   
+        <li>
+            <a href="">conditions</a></li>
+      </ul>
+    </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <h1>fzfkkk</h1>
 
 
         <form action="data-processing.php" method="post">
-            <input type="text" name="identifiant" placeholder="Identifiant"> <br>
-            
-            <input type="radio" name="sexe" value="F" id="Femme">femme
-            <input type="radio" name="sexe" id="H" value="Homme">homme <br>
+            <input type="text" name="pseudo" placeholder="Pseudo"> <br>
+            <input type="text" name="nom" placeholder="Nom"> <br>
+            <input type="text" name="prenom" placeholder="Prénom"> <br>
 
             <input type="email" name="email" placeholder="Email"> <br>
 
             <input type="password" name="password" placeholder="Mot de passe">
-            <input type="password" name="password2" placeholder="Répetez votre mot de passe">
+            <input type="password" name="password2" placeholder="Répetez votre mot de passe"> <br>
 
-            <input type="text" name="telephone" placeholder="Téléphone"> <br>
-
-            <select name="pays">
-                <option value="France">France</option>
-                <option value="France">Russie</option>
-                <option value="France">Espagne</option>
-            </select> <br>
-
-            <input type="checkbox" name="cgu">Conditions générales d'utilisation <br>
-
-            <button type="submit" name="submit">S'inscrire</button>
+            <button type="submit" name="action" value="mailer">S'inscrire</button>
+        
 
         </form>
 
+    <?php
+        if (isset($_SESSION["mailok"])){
+            echo $_SESSION["mailok"];
+            unset($_SESSION["mailok"]);
+        }
+    ?>
     
 </body>
 </html>
