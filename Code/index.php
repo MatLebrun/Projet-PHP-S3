3bd553d0930e7e7c78ded44b21cb6de1ae796476
MatLebrun <mathieu.lebrun@etu.univ-amu.fr>
2021-01-15 13:39:07 +0100

    index et css

---
+++ Code/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Bienvenue sur le réseau social de Vanestar" />
+
+    <link href="" rel="stylesheet">
+    <title>Accueil : Vanestar</title>
+    <link rel="icon" href="">
+
+    
+</head>
+<body>
+    
+</body>
+</html>
