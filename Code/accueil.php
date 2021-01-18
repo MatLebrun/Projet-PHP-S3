247734f60f0ca558e152a0128dce83e02a25040a
MatLebrun <mathieu.lebrun@etu.univ-amu.fr>
2021-01-18 16:17:58 +0100

    Merge branch 'main' of https://github.com/MatLebrun/Projet-PHP-S2 into main

--- Code/accueil.php
+++ Code/accueil.php
@@ -5,17 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Bienvenue sur le réseau social de Vanestar" />
 
-    <link href="./styles/index.css" rel="stylesheet">
-
-    <title>Accueil : Vanestar</title>
-    <link rel="icon" href="">
-
-    
+    <link href="styles/common.css" rel="stylesheet">
+    <title>Accueil : Vanestarre</title>
+    <link rel="icon" href="">   
 </head>
+
 <body>
-    <h1>
-        zgsrgerg
-    </h1>
+
+<div class="topnav">
+  <a href="#home">Vanestarre</a>
+  <a><form>
+  <input class="tagsearch" type="text" placeholder="Recherche">
+  </form></a>
+  <a href="#login">Se connecter</a>
+</div>
+<div class="content">
+
+</div>
+
 
 </body>
 </html>
