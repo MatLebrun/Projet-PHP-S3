2740d14635d8223ff3781d4820bbf52ee182c423
MatLebrun <mathieu.lebrun@etu.univ-amu.fr>
2021-01-20 14:10:35 +0100

    up

--- Code/accueil.php
+++ Code/accueil.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Bienvenue sur le réseau social de Vanestar" />
 
     <link href="styles/common.css" rel="stylesheet">
@@ -11,18 +11,39 @@
 </head>
 
 <body>
+  <div id="page">
+    <div id="main">
+      <nav id="header">
+        <div class="header">
+          <a href="#home">Vanestarre</a>
+          <a><form>
+            <input class="tagsearch" type="text" placeholder="Rechercher">
+          </form></a>
+          <a href="#login">Se connecter</a>
+        </div>
+      </nav>
+      
 
-<div class="topnav">
-  <a href="#home">Vanestarre</a>
-  <a><form>
-  <input class="tagsearch" type="text" placeholder="Recherche">
-  </form></a>
-  <a href="#login">Se connecter</a>
-</div>
-<div class="content">
-
-</div>
+        
 
 
+        <div id="contenu">
+          <div id="texte">
+            <h2>Accueil</h2>
+            <a class="twitter-timeline" href="https://twitter.com/AttaqueTitansFR?ref_src=twsrc%5Etfw"></a>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </div>
+          <div id="sidebar">
+
+          </div>
+        </div><!-- contenu -->
+      </div><!-- fin main -->
+
+      <div class="menu-phone">
+        <a href="#home">&#127968</a>
+        <a href="#search">&#128269</a>
+        <a href="#notififications">&#128276</a>
+    </div>
+  </div>
 </body>
 </html>
