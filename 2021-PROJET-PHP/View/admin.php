1a77878d8ee12514335105b1f0389b124529d46d
MatLebrun <mathieu.lebrun@etu.univ-amu.fr>
2021-01-28 17:08:12 +0100

    admin css

--- 2021-PROJET-PHP/View/admin.php
+++ 2021-PROJET-PHP/View/admin.php
@@ -3,38 +3,68 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Administration</title>
+
+    <link rel="icon" href="../Public/pictures/Icon_Vanestar.png">   
+
+    <link href="../Public/CSS/common.css"  rel="stylesheet" />
+    <link href="../Public/CSS/recuperation.css" rel="stylesheet">
+    <link href="../Public/CSS/header.css"  rel="stylesheet" />
+    <link href="../Public/CSS/footer.css"  rel="stylesheet" />
+
 </head>
-<body>
-    
-<main>
-    <?php for($i = 0; $i < sizeof($listUsers); ++$i) {?>
-
-        <div class="line">
-            <p>
-                Pseudo <?= $listUsers[$i]->getPseudo(); ?> <br>
-                Email <?= $listUsers[$i]->getMail(); ?>
-            </p>
-
-
-            <div>
-                <form action="" method="post">
-                    <input type="hidden" name="userId" value="<?=$listUsers[$i]->getId();?>">
-
-
-                    <button type="submit" name="submit">Supprimer l'utilisateur</button>
-
-                </form>
+    <body>
+        
+        <div id="menu">
+            <div id="main">
+                <nav>
+                    <div class="header">
+                        <a href="./accueil.php">Vanestarre</a>
+                    </div>
+                </nav>
             </div>
-
-
         </div>
 
+        <main>
 
-    <?php } ?>
+            <div class="form-admin">
+            
+                <h2 id="title-admin">Administration</h2>
 
-</main>
+                <?php for($i = 0; $i < sizeof($listUsers); ++$i) {?>
+
+                    <div class="line">
+                        <p>
+                            Pseudo <?= $listUsers[$i]->getPseudo(); ?> <br>
+                            Email <?= $listUsers[$i]->getMail(); ?>
+                        </p>
+
+                        <div>
+                            <form action="" method="post">
+                                <input type="hidden" name="userId" value="<?=$listUsers[$i]->getId();?>">
+
+                                <button type="submit" name="submit">Supprimer l'utilisateur</button>
+                            </form>
+                        </div>
+                    </div>
+
+                <?php } ?>
+                
+            </div>
+            
+        </main>
+
+        <footer>
+            <div class="footer">
+                <img id="logo" src="../Public/pictures/Logo_Vanestar.png" alt="">
+                <p id="copyright">
+                    © 2021 Vanestarre | Tous droits réservés
+                </p>
+            </div>
+        </footer>
 
 
-</body>
+
+
+    </body>
 </html>
